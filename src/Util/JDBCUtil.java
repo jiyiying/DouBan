481e33e332649ea4c21bd080571cc696c7ec9ed8
KiRaKiRa <44049685+Leyfch@users.noreply.github.com>
2019-11-13 17:56:00 +0800

    add DaoFactory and JDBCUtil

---
+++ src/Util/JDBCUtil.java
@@ -0,0 +1,38 @@
+package Util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCUtil {
+	private static String user = "";
+	private static String password = "";
+	private static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	private static String url = "jdbc:sqlserver://127.0.0.1:1433;databaseName=xxxx;";
+
+	static {
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, user, password);
+	}
+
+	public static void closeAll(Connection conn, Statement stmt, ResultSet rs) throws SQLException {
+		if (rs != null) {
+			rs.close();
+		}
+		if (stmt != null) {
+			stmt.close();
+		}
+		if (conn != null) {
+			conn.close();
+		}
+	}
+
+}
