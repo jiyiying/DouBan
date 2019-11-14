fef5c61675666e8cec46a31892ca2ee6505a4827
KiRaKiRa <44049685+Leyfch@users.noreply.github.com>
2019-11-14 19:35:42 +0800

    add UserTest

---
+++ src/Test/User/LoginTest.java
@@ -0,0 +1,36 @@
+package Test.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import DAO.UserDao;
+import Factory.DaoFactory;
+import Model.User;
+
+public class LoginTest {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String userName = scan.next();
+		String password = scan.next();
+		UserDao userdao = DaoFactory.getUserDaoImpl();
+
+		User user = new User();
+		user.setUserName(userName);
+		user.setPassword(password);
+
+		try {
+			ResultSet rs = userdao.search("SELECT * FROM [user] WHERE userName =\'" + user.getUserName()
+					+ "\' AND password = \'" + user.getPassword() + "\'");
+
+			if(!rs.next()) {
+				System.out.println("用户名或密码错误!");
+			}else {
+				System.out.println("登录成功！"+"登录用户："+rs.getString("userName"));
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/Test/User/RegisterTest.java
@@ -0,0 +1,53 @@
+package Test.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import Model.*;
+import DAO.*;
+import Factory.DaoFactory;
+
+public class RegisterTest {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String userName=scan.next();
+		String password=scan.next();
+		String phoneNumber=scan.next();
+		UserDao userdao=DaoFactory.getUserDaoImpl();
+		
+		User user=new User();
+		user.setUserName(userName);
+		user.setPassword(password);
+		user.setPhoneNumber(phoneNumber);
+		
+		try {
+			ResultSet rs=userdao.search("SELECT *" 
+					+"FROM [user]"
+					+"WHERE userName = \'"
+					+user.getUserName()+"\'");
+			
+			if(rs.next()&&rs.getString("userName").equals(user.getUserName())) {
+				System.out.println("用户名已存在");
+			}
+				
+			rs=userdao.search("SELECT *" 
+					+"FROM [user]"
+					+"WHERE phoneNumber = \'"
+					+user.getPhoneNumber()+"\'");
+			
+			if(rs.next()&&rs.getString("phoneNumber").equals(user.getPhoneNumber())) {
+				System.out.println("该手机号已绑定");
+			}else {
+				userdao.insert(user);
+			}
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+
+	}
+
+}
