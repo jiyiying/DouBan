0a69b15aac98583a43fa5ad046c0e8f9445f9db1
KiRaKiRa <44049685+Leyfch@users.noreply.github.com>
2019-11-19 22:07:59 +0800

    show shortreview

---
+++ src/Test/ShortReview/ShortReviewTest.java
@@ -0,0 +1,40 @@
+package Test.ShortView;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
+import DAO.ReviewDao;
+import Factory.DaoFactory;
+import Model.ShortReview;
+
+public class ShortReviewTest {
+	public static void main(String[] args) throws SQLException {
+		ReviewDao shortreviewdao=DaoFactory.getReviewDaoImpl();
+	
+		String sql="select * " + 
+				"from shortreview join movie " + 
+				"on shortreview.movieID = movie.movieID " + 
+				"where movie.movieName = '哪吒之魔童降世' " ;
+		ResultSet rs=shortreviewdao.search(sql);
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+		
+		
+		while(rs.next()) {
+			if(!rs.getBoolean("visibility")) {
+				continue;
+			}
+			
+			System.out.println("用户ID："+rs.getString("userID"));
+			if(rs.getBoolean("wantToWatch")) {
+				System.out.println("想看");
+			}else {
+				System.out.println("看过");
+				System.out.println("评分："+rs.getDouble("score"));
+			}
+			System.out.println(sdf.format(rs.getDate("date")));
+			System.out.println();
+		}
+		
+	}
+}
